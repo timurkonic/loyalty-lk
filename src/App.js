4e6178372c3147a1f4b9f745ac8137a6f82fcd27
Timur Konic <timur.konic@gmail.com>
2022-07-05 15:24:43 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import logo from './img/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="logo"><img src={logo} width="300px" alt="ЛИНИЯ"/></div>
+    </div>
+  );
+}
+
+export default App;
