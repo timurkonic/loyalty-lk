5b62bfd9b0e488eb2bf6f4984b1ad24a9ff37346
Timur Konic <timur.konic@gmail.com>
2022-07-06 08:43:23 +0300

    Check login valid

--- src/Login.js
+++ src/Login.js
@@ -1,33 +1,39 @@
 import { useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 
-function Login() {
+const Login = () => {
 
     const [show, setShow] = useState(true);
+    const [login, setLogin] = useState("");
+    const [loginEnabled, setLoginEnabled] = useState(false);
+    const [password, setPassword] = useState("");
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const checkLogin = (value) => {
+        const re = /^[0-9]{13}$/;
+        setLoginEnabled(re.test(value));
+        setLogin(value);
+    }
 
     return (
         <>
-            <Modal show={show} size="lg" backdrop={false} centered={true} animation={false} onHide={handleClose} contentClassName="border-0 shadow-lg">
+            <Modal show={show} size="lg" backdrop={false} centered={true} animation={false} keyboard={false} onHide={() => setShow(false)} contentClassName="border-0 shadow-lg">
                 <Modal.Body>
                     <Form>
                         <Form.Group>
                             <Form.Label>Номер карты</Form.Label>
-                            <Form.Control id="login"/>
+                            <Form.Control id="login" value={login} onChange={e => checkLogin(e.target.value)}/>
                             <Form.Text className="text-muted">Тринадцатизначный номер Вашей карты лояльности Народная, находится на обратной стороне</Form.Text>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Пароль</Form.Label>
-                            <Form.Control type="password"/>
+                            <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)}/>
                             <Form.Text className="text-muted">Пароль для входа в личный кабинет</Form.Text>
                         </Form.Group>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="red" onClick={handleClose}>Войти</Button>
-                    <Button variant="blue" onClick={handleClose}>Регистрация</Button>
+                    <Button variant="red" onClick={() => setShow(false)} disabled={!loginEnabled}>Войти</Button>
+                    <Button variant="blue" onClick={() => setShow(false)}>Регистрация</Button>
                 </Modal.Footer>
             </Modal>
       </>
